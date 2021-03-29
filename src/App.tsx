3ec47cb2738da7753dd0ff978273466d6a9c6126
Victor <victor.h.m.alves@gmail.com>
2021-03-29 14:20:19 +0200

    Component Content

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
- import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
+import { GenreResponseProps } from './services/types';
 
 import './styles/global.scss';
-
-import './styles/sidebar.scss';
-import './styles/content.scss';
-
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
 
 interface MovieProps {
   imdbID: string;
@@ -66,19 +55,10 @@ export function App() {
         selectedGenreId={selectedGenreId}  
       />
 
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content 
+        selectedGenre={selectedGenre}
+        movies={movies}
+      />
     </div>
   )
 }
